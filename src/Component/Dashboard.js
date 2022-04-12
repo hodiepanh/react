ce496d46af7f2240ac4babf9bbc7a35abe8cb2c4
hodiepanh <hodiepanh1200@gmail.com>
2022-04-12 14:44:10 +0700

    (1) Fix error in console (2) add debounce search

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -5,8 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
 import { ItemContext } from "../Store/Context";
-import './Dashboard.css';
-
+import "./Dashboard.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,35 +16,35 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: theme.palette.text.secondary,
     aspectRatio: 1 / 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightgrey',
-    '&:hover':{
-      backgroundColor: 'black'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightgrey",
+    "&:hover": {
+      backgroundColor: "black",
     },
-    '& .item-button-wrapper': {
-      display: 'none',
+    "& .item-button-wrapper": {
+      display: "none",
     },
-    '&:hover .item-button-wrapper': {
-      display: 'flex',
+    "&:hover .item-button-wrapper": {
+      display: "flex",
     },
-    '&:hover .item-button-wrapper .edit-button': {
-      backgroundColor:'white',
-      '&:hover':{
-        border: '1px solid white',
-        backgroundColor:'transparent',
-        color: 'white',
+    "&:hover .item-button-wrapper .edit-button": {
+      backgroundColor: "white",
+      "&:hover": {
+        border: "1px solid white",
+        backgroundColor: "transparent",
+        color: "white",
       },
       margin: 10,
     },
 
-    '&:hover .item-button-wrapper .remove-button': {
-      backgroundColor: 'red',
-      '&:hover':{
-        border: '1px solid red',
-        backgroundColor:'transparent',
-        color: 'red',
+    "&:hover .item-button-wrapper .remove-button": {
+      backgroundColor: "red",
+      "&:hover": {
+        border: "1px solid red",
+        backgroundColor: "transparent",
+        color: "red",
       },
       margin: 10,
     },
@@ -72,31 +71,35 @@ function Dashboard() {
     setItemValue(newItemValue);
   };
 
-  const searchItem = (event) => {
-    if (event.key === "Enter") {
+  useEffect(() => {
+    const delaySearch = setTimeout(() => {
       if (searchValue !== "") {
         const searchItem = itemValue.filter((items) =>
           items.title.includes(searchValue)
         );
-
         setItemMap(searchItem);
+      } else {
+        setItemMap(itemValue);
       }
-    } else {
-      setItemMap(itemValue);
-    }
-  };
+    }, 500);
+    return () => clearTimeout(delaySearch);
+  }, [searchValue]);
 
   const itemCard = itemMap.map((data, index) => (
-    <Grid className = "item-wrapper" item xs={3}>
-      <Paper className= {classes.paper} key={data.id}>
-        <div className="item-button-wrapper">
-        <Button className="edit-button"
-        onClick={() => editItem(index)}>Edit</Button>
-        <Button className="remove-button"
-        onClick={() => removeItem(index)}>Remove</Button>
-        </div>
-      </Paper>
-      <div className="item-title">{data.title}</div>
+    <Grid className="item-wrapper" item xs={3} key={index}>
+      <li key={index} style={{ listStyleType: "none" }}>
+        <Paper className={classes.paper}>
+          <div className="item-button-wrapper">
+            <Button className="edit-button" onClick={() => editItem(index)}>
+              Edit
+            </Button>
+            <Button className="remove-button" onClick={() => removeItem(index)}>
+              Remove
+            </Button>
+          </div>
+        </Paper>
+        <div className="item-title">{data.title}</div>
+      </li>
     </Grid>
   ));
 
@@ -104,28 +107,30 @@ function Dashboard() {
     <div>
       <div className={classes.root}>
         <div className="add-button-wrapper">
-        <Button style = {{color: 'royalblue'}}
-          onClick={() => {
-            history.push("/create");
-          }}
-        >
-          Add new item
-        </Button>
+          <Button
+            style={{ color: "royalblue" }}
+            onClick={() => {
+              history.push("/create");
+            }}
+          >
+            Add new item
+          </Button>
         </div>
         <div className="searchbar-wrapper">
-        <input className="searchbar"
-          type="text"
-          placeholder="Type something to search"
-          onChange={(e) => {
-            setSearchValue(e.target.value);
-          }}
-          onKeyDown={searchItem}
-        ></input>
+          <input
+            className="searchbar"
+            type="text"
+            placeholder="Type something to search"
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            //onKeyDown={searchItem}
+          ></input>
         </div>
         <div className="grid-wrapper">
-        <Grid container spacing={3}>
-          {itemCard}
-        </Grid>
+          <Grid container spacing={3}>
+            {itemCard}
+          </Grid>
         </div>
       </div>
     </div>
