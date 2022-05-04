7ea9d056d154ced51e1c161dd1b2bf8fde78f669
hodiepanh <hodiepanh1200@gmail.com>
2022-05-04 17:12:17 +0700

    fix eslint error

--- redux-dashboard/src/App.js
+++ redux-dashboard/src/App.js
@@ -1,42 +1,31 @@
 import React from "react";
-import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Create from "./component/Create";
 import Edit from "./component/Edit";
 import HeaderBar from "./component/HeaderBar";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchItem } from "./feature/Item";
 function App() {
-  const axios = require("axios");
-  const dispatch = useDispatch();
-  // useEffect(() => {
-  //   axios.get("http://localhost:3001/posts").then((response) => {
-  //     dispatch(fetchItem(response.data));
-  //   });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-  return (
-    <div>
-      <HeaderBar />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/create">
-            <Create />
-          </Route>
-          <Route path="/edit/:id">
-            <Edit />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<HeaderBar />
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Dashboard />
+					</Route>
+					<Route exact path="/dashboard">
+						<Dashboard />
+					</Route>
+					<Route exact path="/create">
+						<Create />
+					</Route>
+					<Route path="/edit/:id">
+						<Edit />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
